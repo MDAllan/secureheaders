1f06f21c3e31e96e4b4336dce8029835e2e57434
emmamcmillin <48865335+emmamcmillin@users.noreply.github.com>
2025-02-19 14:13:03 -0700

    adding in empty file structure

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,4 @@
+start("c:\\Users\\madih\\public\\secureheaders\\routes\\");
+onHasParentDirectory();
+addRow("posts.js","posts.js",0,352,"352 B",1739862513,"2/18/25, 12:08:33 AM");
+addRow("user.js","user.js",0,791,"791 B",1739862513,"2/18/25, 12:08:33 AM");
