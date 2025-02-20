baf8963e367f0349a1204e56b26ac4fb66b63993
emmamcmillin <48865335+emmamcmillin@users.noreply.github.com>
2025-02-19 19:51:33 -0700

    moving ssl config and helmet config to their own routing files

--- backend/.env/server.js
+++ backend/.env/server.js
@@ -11,35 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // we configure SSL certificates for development using self-signed certificates.
 // For production, I  would suggest relying on a trusted Certificate Authority.
-const options = {
-  key: fs.readFileSync('certs/private.key'),
-  cert: fs.readFileSync('certs/certificate.crt'),
-};
+const sslOptions = require('./config/sslConfig');
 
 // I’ve implemented secure HTTP headers using Helmet to strengthen the app's security.
-app.use(helmet({
-  // Content Security Policy (CSP) helps prevent XSS attacks by restricting the sources of content.
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"], // Only allow content from the same origin
-      scriptSrc: ["'self'"], // Removed 'unsafe-inline' for enhanced security
-      objectSrc: ["'none'"], // Prevent embedding objects like Flash
-      upgradeInsecureRequests: [], // Forces upgrading HTTP to HTTPS automatically to prevent mixed content
-    }
-  },
-  // X-Frame-Options with 'deny' prevents clickjacking by blocking embedding of the site in an iframe
-  frameguard: { action: 'deny' },
-  // I hide the X-Powered-By header to avoid revealing unnecessary server details.
-  hidePoweredBy: true,
-  // I prevent MIME sniffing to avoid script injection attacks.
-  noSniff: true,
-  // Enabling XSS filter for browsers to mitigate cross-site scripting attacks.
-  xssFilter: true,
-  // The referrer policy ensures referrer information is sent only with secure requests.
-  referrerPolicy: { policy: 'strict-origin-when-cross-origin' },
-  // Cross-Origin Resource Policy restricts sharing resources between origins.
-  crossOriginResourcePolicy: { policy: 'same-origin' }
-}));
+const helmetConfig = require('./config/helmetConfig');
+app.use(helmetConfig);
+
 
 // I’ve implemented a caching strategy for static content to improve performance.
 app.use((req, res, next) => {
