8fd2437b8537608aa5881b241bd2178a835c2fc0
emmamcmillin <48865335+emmamcmillin@users.noreply.github.com>
2025-02-26 09:50:32 -0700

    installing new packages from password hashing lab and inputting code from lab for user and authroutes

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/userAuth', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected!"))
+.catch(err => console.log(err));
+
+//import routes
+const authRoutes = require('./routes/auth');
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server 
+running on port ${PORT}`));
